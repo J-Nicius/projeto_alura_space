45e61e908619a75f5416eb9eadf90b0930b9ce5f
Joao Vinicius de Santana Santos <joao.santos@solutis.net.br>
2025-09-04 11:16:05 -0300

    projeto Alura Space

---
+++ galeria/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Olá, mundo! Esta é a página inicial da galeria.")
+
+# Create your views here.
